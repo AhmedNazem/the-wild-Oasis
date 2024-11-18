658c09c338e056e2c3caa769813e8c1fcf3232dd
AhmedNazem <aahhaammdd55@gmail.com>
2024-11-18 03:52:20 +0300

    using toaster for popup messages in delete cabin

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,14 +13,23 @@ const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
 function AppLayout() {
   return (
     <StyledAppLayOut>
       <Header />
       <Sidebar />
-      <Main>
-        <Outlet />
-      </Main>
+      <Container>
+        <Main>
+          <Outlet />
+        </Main>
+      </Container>
     </StyledAppLayOut>
   );
 }
